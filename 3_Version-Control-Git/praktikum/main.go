6fff27ac49125f0a92c5e5dce9d296163c508a27
Your Name <you@example.com>
2023-08-21 09:02:31 +0700

    Update

--- 3_Version-Control-Git/praktikum/main.go
+++ 3_Version-Control-Git/praktikum/main.go
@@ -1,9 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello World!")
-	fmt.Println("Laode Saady")
-	fmt.Println("Saady")
+	var palindrom string
+	fmt.Printf("Input Disini : ")
+	fmt.Scan(&palindrom)
+	b := true
+	for i := 0; i < len(palindrom)/2; i++ {
+		a := palindrom[i]
+		rumus := palindrom[len(palindrom)-i-1]
+
+		if a != rumus {
+			b = false
+			break
+		}
+	}
+	if b {
+		fmt.Println("Ini kata Palindrom")
+		// break
+	} else {
+		fmt.Println("Bukan Palindrom")
+		// break
+	}
 }
