094e1a31b7b67c40abe0ffa319ac2a96baec9f67
Your Name <you@example.com>
2023-08-26 08:26:52 +0700

    Update

--- 5_Data-Structure/pratikum/soal-prioritas1/nomor1.go
+++ 5_Data-Structure/pratikum/soal-prioritas1/nomor1.go
@@ -5,28 +5,24 @@ import "fmt"
 func ArrayMarge(ArrayA, ArrayB []string) []string {
 	var result []string
 	cek := make(map[string]bool)
-	a := ArrayA
-	b := ArrayB
-	// fmt.Println(a)
-	// fmt.Println(b)
 
-	for _, va := range a {
-		result = append(result, va)
-		cek[va] = true
+	for _, v := range ArrayA {
+		result = append(result, v)
+		cek[v] = true
 	}
-	for _, vb := range b {
+
+	for _, vb := range ArrayB {
 		if cek[vb] == false {
 			result = append(result, vb)
 		}
 	}
-
 	return result
 }
 
 func main() {
 	// fmt.Println(ArrayMarge([]string{"King","Devil Jin", "Akuma"},[]string{"eddie","steve", "geese"}))
-	// fmt.Println(ArrayMarge([]string{"segei", "jin"}, []string{"jin", "bryan"}))
-	fmt.Println(ArrayMarge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa", "law"}))
+	fmt.Println(ArrayMarge([]string{"segei", "jin"}, []string{"jin", "bryan"}))
+	// fmt.Println(ArrayMarge([]string{"alisa", "yoshimitsu"}, []string{"devil jin", "yoshimitsu", "alisa", "law"}))
 	// fmt.Println(ArrayMarge([]string{}, []string{"devil jin", "sergei"}))
 	// fmt.Println(ArrayMarge([]string{"hwoarang"}, []string{}))
 	// fmt.Println(ArrayMarge([]string{}, []string{}))
---
+++ 5_Data-Structure/pratikum/soal-prioritas2/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func PairSum(arr []int, target int) []int {
+	var result []int
+
+	
+
+	return result 
+}
+
+func main() {
+
+	// Test cases
+
+	fmt.Println(PairSum([]int{1, 2, 3, 4, 6}, 6)) // [1, 3]
+
+	fmt.Println(PairSum([]int{2, 5, 9, 11}, 11)) // [0, 2]
+
+	fmt.Println(PairSum([]int{1, 3, 5, 7}, 12)) // [2, 3]
+
+	fmt.Println(PairSum([]int{1, 4, 6, 8}, 10)) // [1, 2]
+
+	fmt.Println(PairSum([]int{1, 5, 6, 7}, 6)) // [0, 1]
+
+}
