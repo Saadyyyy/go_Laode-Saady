0992402e8347a976f6cea66c982d08bdc51f2ecc
Your Name <you@example.com>
2023-09-04 06:56:06 +0700

    update

--- 8_Problem-Solving-Paradigm–Brute-Force-Greedy-and-Dynamic-Programming/praktikum/Prioritas-2/nomor1.go
+++ 8_Problem-Solving-Paradigm–Brute-Force-Greedy-and-Dynamic-Programming/praktikum/Prioritas-2/nomor1.go
@@ -1,18 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func Frog(jumps []int) int {
-	var result int
-	for i := 0; i < len(jumps); i++ {
-		for j := 0; j < i; j++ {
-			result += j
-		}
+func minCostFrog(jumps []int) int {
+	n := len(jumps)
+	if n <= 1 {
+		return 0
 	}
-	return result
+
+	minCost := make([]int, n)
+
+	minCost[0] = 0
+	minCost[1] = abs(jumps[1] - jumps[0])
+
+	for i := 2; i < n; i++ {
+		minCost[i] = min(minCost[i-1]+abs(jumps[i]-jumps[i-1]), minCost[i-2]+abs(jumps[i]-jumps[i-2]))
+	}
+
+	return minCost[n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 func main() {
-	fmt.Println(Frog([]int{10, 30, 40, 20}))         // 30
-	fmt.Println(Frog([]int{30, 10, 60, 10, 60, 50})) // 40
+	fmt.Println(minCostFrog([]int{10, 30, 40, 20}))         // Output: 30
+	fmt.Println(minCostFrog([]int{30, 10, 60, 10, 60, 50})) // Output: 40
 }
