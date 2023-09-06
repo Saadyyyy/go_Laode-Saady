41713ec2548081a830db98b0939238c395b08b28
Your Name <you@example.com>
2023-09-06 10:23:25 +0700

    Update Tugas Clean Code

--- 10_Clean-code/prioritas-1/nomor1.go
+++ 10_Clean-code/prioritas-1/nomor1.go
@@ -1,23 +1,27 @@
 package main
 
-import "fmt"
-
 type user struct {
 	id       int
-	username int // username bertipe int yang dimana kita tau kalau username bertipe string agar bisa di isi dengan value nama harusnya username bertipe string
+	username int // username bertipe int yang dimana kita tau kalau username bertipe string agar bisa di isi dengan value nama
 	password int
 }
 
-// kita membuat lagi struct
+// Untuk penamaan Struct nya lebih baik menggunakan Camel case
+// userService
 type userservice struct {
 	t []user
 }
 
-func (u userservice) getallusers() []user {
+// Penamaan pada method ini perlu di perbaiki karna tidak menetapkan clean code
+// disini menurut saya meode yang cocok di gunakan adalah Camel case
+func (u userservice) getallusers() []user { // getAllUsers
 	return u.t
 }
 
-func (u userservice) getuserbyid(id int) user {
+// Penamaan pada method ini perlu di perbaiki karna tidak menetapkan clean code
+// disini menurut saya meode yang cocok di gunakan adalah Camel case
+func (u userservice) getuserbyid(id int) user { // getUserById
+	// untuk penamaan r kurang tepat karna susah membacanya lebih baik di ganti dengan `value`
 	for _, r := range u.t {
 		if id == r.id {
 			return r
@@ -25,14 +29,4 @@ func (u userservice) getuserbyid(id int) user {
 	}
 
 	return user{}
-}
-
-func main() {
-	u := user{
-		id:       20,
-		username: 334,
-		password: 37382,
-	}
-
-	fmt.Println(u)
 }
--- 10_Clean-code/prioritas2/nomor1.go
+++ 10_Clean-code/prioritas2/nomor1.go
@@ -1,8 +1,11 @@
 package main
 
+import "fmt"
+
 type kendaraan struct {
-	totalroda       int
-	kecepatanperjam int
+	// disini saya merubah dari totalroda = totalRoda
+	// totalRoda       int // saya juga mengomentari juga totalRoda karna totalRoda tidak di gunakan
+	kecepatanPerJam int // disini saya merubah dari kecepatanperjam = kecepatanPerJam
 }
 
 type mobil struct {
@@ -10,19 +13,26 @@ type mobil struct {
 }
 
 func (m *mobil) berjalan() {
-	m.tambahkecepatan(10)
+	m.tambahKecepatan(10)
 }
 
-func (m *mobil) tambahkecepatan(kecepatanbaru int) {
-	m.kecepatanperjam = m.kecepatanperjam + kecepatanbaru
+// function ini mungkin susah di baca karna tidak menerapkan Camel case jadi saya ubah dari kecepatankecapatan = kecapatanKecepatan
+func (m *mobil) tambahKecepatan(kecepatanBaru int) { // dan saya mengubah parameter dari kecepatanbaru = kecepatanBaru
+	m.kecepatanPerJam = m.kecepatanPerJam + kecepatanBaru
 }
 
 func main() {
-	mobilcepat := mobil{}
-	mobilcepat.berjalan()
-	mobilcepat.berjalan()
-	mobilcepat.berjalan()
+	//agar variable baru dari struct mobil lebih simple jadi saya ubah dari mobilcepat = cepat
+	cepat := mobil{}
+	cepat.berjalan()
+	cepat.berjalan()
+	cepat.berjalan()
+	// saya sedikit menambah fmt.Println agar kita bisa di tampilkan cepat dari kecepatanPerjam
+	fmt.Println("Cepat Mobil :", cepat.kecepatanPerJam)
 
-	mobillamban := mobil{}
-	mobillamban.berjalan()
+	//agar variable baru dari struct mobil lebih simple jadi saya ubah dari mobillamban = lamban
+	lamban := mobil{}
+	lamban.berjalan()
+	// saya sedikit menambah fmt.Println agar kita bisa di tampilkan lamban dari kecepatanPerjam
+	fmt.Println("Lamban Mobil :", lamban.kecepatanPerJam)
 }
